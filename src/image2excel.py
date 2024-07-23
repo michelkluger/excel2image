bf4ddd4f630ee588ae39e34b6341fdd93f4eb5cf
Michel Klueger <klueger@ecoinvent.org>
2024-07-23 09:48:02 +0200

    chore: Refactor image2excel.py for improved readability and maintainability

--- src/image2excel.py
+++ src/image2excel.py
@@ -1,50 +1,58 @@
+import os
+
 import numpy.lib.recfunctions as nlr
 import pandas as pd
-import os
 from colormap import rgb2hex
 from matplotlib.image import imread
-from PIL import Image
 from openpyxl import load_workbook
+from PIL import Image
 
-def __default_path(fileName):
+
+def __default_path(fileName: str) -> str:
     path = os.getcwd()
-    file = os.path.join(path,fileName)
+    file = os.path.join(path, fileName)
     return file
 
-def __colorCells(string):
-    return 'background-color:' + string
 
-def __changeZoom(excelFile,zoom=10):
+def __colorCells(string: str) -> str:
+    return "background-color:" + string
+
+
+def __changeZoom(excelFile: str, zoom: int = 10) -> None:
     wb = load_workbook(excelFile)
     for ws in wb.worksheets:
         ws.sheet_view.zoomScale = zoom
     wb.save(excelFile)
 
-def __resize_picture(file,keep_aspect,extension):
+
+def __resize_picture(file: str, keep_aspect: bool, extension: str) -> str:
     image = Image.open(file)
-    if(sorted(image.size)[0]> 260 or sorted(image.size)[1]> 300):
+    if sorted(image.size)[0] > 260 or sorted(image.size)[1] > 300:
         if keep_aspect:
-            image.thumbnail(tuple([300/max(image.size)*x for x in image.size]), Image.ANTIALIAS)
+            image.thumbnail(
+                tuple([300 / max(image.size) * x for x in image.size]), Image.ANTIALIAS
+            )
         else:
-            image = image.resize((260,300),Image.ANTIALIAS)
-        file=file.replace(extension,"_small" + extension)
+            image = image.resize((260, 300), Image.ANTIALIAS)
+        file = file.replace(extension, "_small" + extension)
         image.save(file)
     return file
 
-def im2xlsx(file,resize=True,keep_aspect=False):
+
+def im2xlsx(file: str, resize: bool = True, keep_aspect: bool = False) -> None:
     path, fileName = os.path.split(file)
     extension = os.path.splitext(file)[1]
-    if (path == ""):
+    if path == "":
         file = __default_path(fileName)
     if resize:
-        file = __resize_picture(file,keep_aspect,extension)
+        file = __resize_picture(file, keep_aspect, extension)
     try:
         img = imread(file)
     except:
         raise Exception
-    df = pd.DataFrame(nlr.unstructured_to_structured(img).astype('O'))
-    df=df.applymap(lambda x: rgb2hex(*x))
-    s=df.style.applymap(lambda x:"background-color:"+str(x))
-    excelFile = file.replace(extension,".xlsx")
-    s.to_excel(excelFile,engine='xlsxwriter')
+    df = pd.DataFrame(nlr.unstructured_to_structured(img).astype("O"))
+    df = df.applymap(lambda x: rgb2hex(*x))
+    s = df.style.applymap(lambda x: "background-color:" + str(x))
+    excelFile = file.replace(extension, ".xlsx")
+    s.to_excel(excelFile, engine="xlsxwriter")
     __changeZoom(excelFile)

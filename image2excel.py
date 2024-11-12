de1a6ae3100cb40b744fc8c2a5c4bb0abad00007
BarbaraLearn <bleblegrr@gmail.com>
2024-11-12 22:26:35 +0100

    First Try - Type Annotations, mypy (#6)
    * test commit
    
    * First try
    
    * delete source
    
    * requirements&mypy in pyproject, add to CI check
    
    * Corrected mypy/ CI
    
    * changes
    
    * xxx
    
    * blablabla
    
    * blablabla 2
    
    * blablabla 3
    
    * blablabla 4
    
    * blablabla 5
    
    * blablabla 6
    
    * blablabla 7
    
    * blablabla 8
    
    * blablabla 9
    
    * blablabla 10
    
    * blablabla 11
    
    * blablabla 12
    
    * Finalised Type Annotations
    
    * add explicit requirements
    
    ---------
    
    Co-authored-by: Michel Klueger <50880307+michelkluger@users.noreply.github.com>
    Co-authored-by: Michel Kluger <michel.kluger@gmail.com>

--- image2excel.py
+++ image2excel.py
@@ -3,35 +3,42 @@ import pandas as pd
 import os
 from colormap import rgb2hex
 from matplotlib.image import imread
-from PIL import Image
+from PIL import Image, ImageFile
 from openpyxl import load_workbook
+from typing import Union
 
-def __default_path(fileName):
+#ignore missing imports for colormap -> mypy.ini ignore_missing_imports = True
+
+def __default_path(fileName : str) -> str:
     path = os.getcwd()
     file = os.path.join(path,fileName)
     return file
 
-def __colorCells(string):
+def __colorCells(string : str) -> str:
     return 'background-color:' + string
 
-def __changeZoom(excelFile,zoom=10):
+def __changeZoom(excelFile : str, zoom : int =10) -> None: # openpyxl.worksheet.worksheet.Worksheet
     wb = load_workbook(excelFile)
     for ws in wb.worksheets:
         ws.sheet_view.zoomScale = zoom
     wb.save(excelFile)
 
-def __resize_picture(file,keep_aspect,extension):
-    image = Image.open(file)
+def __resize_picture(file : str, keep_aspect : bool, extension : str) -> str:
+    image: Union[Image.Image, ImageFile.ImageFile]  = Image.open(file)
     if(sorted(image.size)[0]> 260 or sorted(image.size)[1]> 300):
         if keep_aspect:
-            image.thumbnail(tuple([300/max(image.size)*x for x in image.size]), Image.ANTIALIAS)
+            width, height = image.size
+            new_size = (300 / max(image.size) * width, 300 / max(image.size) * height)
+            image.thumbnail(new_size, Image.Resampling.LANCZOS)
+            #image.thumbnail(tuple([300/max(image.size)*x for x in image.size]), Image.Resampling.LANCZOS) #Image.ANTIALIAS depricated
+            #getting error cause the image can be over 2D
         else:
-            image = image.resize((260,300),Image.ANTIALIAS)
+            image = image.resize((260,300), Image.Resampling.LANCZOS) #Image.ANTIALIAS depricated
         file=file.replace(extension,"_small" + extension)
         image.save(file)
     return file
 
-def im2xlsx(file,resize=True,keep_aspect=False):
+def im2xlsx(file : str,  resize : bool =True, keep_aspect : bool =False) -> None:
     path, fileName = os.path.split(file)
     extension = os.path.splitext(file)[1]
     if (path == ""):
